8c191d8f503b969f9d767e56c89d2bbdde2a350d
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-06 15:35:51 +0700

    Mengisi class siswa dengan private dan public

--- file4.cpp
+++ file4.cpp
@@ -9,5 +9,16 @@ private:
     string nama;
 
 public:
-void setNama(string pNama);
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId (int pId);
+    void displayAll(orang &a);
 };
