9e4a243ec9ec25e6b5ef1102ddfc3c18459d7240
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-06 15:40:01 +0700

    Menambahkan void siswa dan void orang dan mengisi int main

--- file4.cpp
+++ file4.cpp
@@ -22,3 +22,22 @@ public:
     void setId (int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+}
